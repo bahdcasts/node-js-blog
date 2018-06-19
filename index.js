49e00475d3eb66c33d42fe177765d65df03752d9
Kati Frantz <frantzenterprises777@gmail.com>
2018-06-19 13:37:13 +0100

    add about, post, and contact pages

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'pages/index.html'))
 })
 
+app.get('/about', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+})
+
+app.get('/post', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+})
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+})
+
 app.listen(4000, () => {
   console.log('App listening on port 4000')
 })
