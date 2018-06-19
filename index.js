effead480b4092dce645a404e1da8d880bc625c5
Kati Frantz <frantzenterprises777@gmail.com>
2018-06-19 13:30:08 +0100

    move assets to public directory create index.html file

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+const path = require('path')
 const express = require('express')
 
 const app = new express()
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+})
 
 app.listen(4000, () => {
   console.log('App listening on port 4000')
