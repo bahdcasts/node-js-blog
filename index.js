78610fa1c52eb2c242d52246812d4013fdf8c4ef
Kati Frantz <frantzenterprises777@gmail.com>
2018-06-19 13:46:57 +0100

    install edge templating engine

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const path = require('path')
+const expressEdge = require('express-edge')
 const express = require('express')
 
 const app = new express()
 
 app.use(express.static('public'))
+app.use(expressEdge)
+app.set('views', `${__dirname}/views`)
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+  res.render('index')
 })
 
 app.get('/about', (req, res) => {
