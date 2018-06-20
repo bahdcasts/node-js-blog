b0030d08f268101e1965fd1deb62c5974f73593e
Kati Frantz <frantzenterprises777@gmail.com>
2018-06-20 09:43:08 +0100

    move post and contact page to use edge templates

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ app.get('/about', (req, res) => {
 })
 
 app.get('/post', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+  res.render('post')
 })
 
 app.get('/contact', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+  res.render('contact')
 })
 
 app.listen(4000, () => {
