7156c643143101584d4a35431e2bcba26b52c9f2
Kati Frantz <frantzenterprises777@gmail.com>
2018-06-25 11:43:41 +0100

    add username and createdAt fields to post collection

--- database/models/Post.js
+++ database/models/Post.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose')
 const PostSchema = new mongoose.Schema({
   title: String,
   description: String,
-  content: String
+  content: String,
+  username: String,
+  createdAt: {
+    type: Date,
+    default: new Date()
+  }
 })
 
 const Post = mongoose.model('Post', PostSchema)
