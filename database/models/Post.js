e03d662a203786ab33eae4c4ac757c40a2851999
Kati Frantz <frantzenterprises777@gmail.com>
2018-06-25 11:49:35 +0100

    renaming description to subtitle

--- database/models/Post.js
+++ database/models/Post.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const PostSchema = new mongoose.Schema({
   title: String,
-  description: String,
+  subtitle: String,
   content: String,
   username: String,
   createdAt: {
