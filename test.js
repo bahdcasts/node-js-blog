bec29853086371ce127e382d38b120103bc43234
Kati Frantz <frantzenterprises777@gmail.com>
2018-06-25 08:50:59 +0100

    fetch single documents from database

--- test.js
+++ test.js
@@ -3,10 +3,12 @@ const Post = require('./database/models/Post')
 
 mongoose.connect('mongodb://localhost/node-js-test-blog')
 
-Post.find({
-  title: 'My first blog post'
-}, (error, posts) => {
-  console.log(error, posts)
+// Post.find({}, (error, posts) => {
+//   console.log(error, posts)
+// })
+
+Post.findById("5b309ceca84e99bbb6601904", (error, post) => {
+  console.log(error, post)
 })
 
 // Post.create({
