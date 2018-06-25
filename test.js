493a60cbc3f3ea5463a0f068a10a2f32625a2632
Kati Frantz <frantzenterprises777@gmail.com>
2018-06-25 09:01:25 +0100

    update database documents

--- test.js
+++ test.js
@@ -11,6 +11,12 @@ Post.find({}, (error, posts) => {
 //   console.log(error, post)
 // })
 
+// Post.findByIdAndUpdate("5b309b35bd7950bab178d912", {
+//   title: 'My first blog post title lorem ipsum'
+// }, (error, post) => {
+//   console.log(error, post)
+// })
+
 // Post.findByIdAndRemove("5b309ceca84e99bbb6601904", (error) => {
 //   console.log(error)
 // })
