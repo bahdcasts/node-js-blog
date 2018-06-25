27f6a48858e2eb62f683d078d8b615602aa198e7
Kati Frantz <frantzenterprises777@gmail.com>
2018-06-25 09:40:54 +0100

    install body-parser package

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const expressEdge = require('express-edge')
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 const app = new express()
 
@@ -11,12 +12,20 @@ app.use(express.static('public'))
 app.use(expressEdge)
 app.set('views', `${__dirname}/views`)
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   res.render('index')
 })
 
 app.get('/posts/new', (req, res) => {
   res.render('create')
+})
+
+app.post('/posts/store', (req, res) => {
+  console.log(req.body)
+  res.redirect('/')
 })
 
 app.get('/about', (req, res) => {
