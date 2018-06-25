a1b179c474e51b28e13c7b80384514b6b48166e8
Kati Frantz <frantzenterprises777@gmail.com>
2018-06-25 13:49:53 +0100

    image upload with express-uploader

--- index.js
+++ index.js
@@ -1,55 +1,61 @@
-const path = require('path')
-const expressEdge = require('express-edge')
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+const path = require("path");
+const expressEdge = require("express-edge");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
 
-const Post = require('./database/models/Post')
+const Post = require("./database/models/Post");
 
-const app = new express()
+const app = new express();
 
-mongoose.connect('mongodb://localhost/node-js-blog')
+mongoose.connect("mongodb://localhost/node-js-blog");
 
-app.use(express.static('public'))
-app.use(expressEdge)
-app.set('views', `${__dirname}/views`)
+app.use(fileUpload());
+app.use(express.static("public"));
+app.use(expressEdge);
+app.set("views", `${__dirname}/views`);
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', async (req, res) => {
-  const posts = await Post.find({})
-  console.log(posts)
-  res.render('index', {
+app.get("/", async (req, res) => {
+  const posts = await Post.find({});
+  console.log(posts);
+  res.render("index", {
     posts
+  });
+});
+
+app.get("/posts/new", (req, res) => {
+  res.render("create");
+});
+
+app.post("/posts/store", (req, res) => {
+  const { image } = req.files
+
+  image.mv(path.resolve(__dirname, 'public/posts', image.name), (error) => {
+    Post.create(req.body, (error, post) => {
+      res.redirect("/");
+    });
   })
-})
+});
 
-app.get('/posts/new', (req, res) => {
-  res.render('create')
-})
+app.get("/about", (req, res) => {
+  res.render("about");
+});
 
-app.post('/posts/store', (req, res) => {
-  Post.create(req.body, (error, post) => {
-    res.redirect('/')
-  })
-})
-
-app.get('/about', (req, res) => {
-  res.render('about')
-})
-
-app.get('/post/:id', async (req, res) => {
-  const post = await Post.findById(req.params.id)
-  res.render('post', {
+app.get("/post/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.render("post", {
     post
-  })
-})
+  });
+});
 
-app.get('/contact', (req, res) => {
-  res.render('contact')
-})
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
 
 app.listen(4000, () => {
-  console.log('App listening on port 4000')
-})
+  console.log("App listening on port 4000");
+});
