7a37870fe2935497b2a66d26a5e64ac9d96f2d2e
Kati Frantz <frantzenterprises777@gmail.com>
2018-06-25 14:06:15 +0100

    add image field to post schema

--- database/models/Post.js
+++ database/models/Post.js
@@ -5,6 +5,7 @@ const PostSchema = new mongoose.Schema({
   subtitle: String,
   content: String,
   username: String,
+  image: String,
   createdAt: {
     type: Date,
     default: new Date()
--- index.js
+++ index.js
@@ -35,7 +35,10 @@ app.post("/posts/store", (req, res) => {
   const { image } = req.files
 
   image.mv(path.resolve(__dirname, 'public/posts', image.name), (error) => {
-    Post.create(req.body, (error, post) => {
+    Post.create({
+      ...req.body,
+      image: `/posts/${image.name}`
+    }, (error, post) => {
       res.redirect("/");
     });
   })
