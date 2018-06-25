64647926ef39fb110818c05181560a80b0ee1a67
Kati Frantz <frantzenterprises777@gmail.com>
2018-06-25 08:44:29 +0100

    finding database racords with mongoose

--- test.js
+++ test.js
@@ -3,10 +3,16 @@ const Post = require('./database/models/Post')
 
 mongoose.connect('mongodb://localhost/node-js-test-blog')
 
-Post.create({
-  title: 'My first blog post',
-  description: 'Blog post description',
-  content: 'Lorem ipsum content.'
-}, (error, post) => {
-  console.log(error, post)
+Post.find({
+  title: 'My first blog post'
+}, (error, posts) => {
+  console.log(error, posts)
 })
+
+// Post.create({
+//   title: 'My second blog post',
+//   description: 'Second Blog post description',
+//   content: 'Second Lorem ipsum content.'
+// }, (error, post) => {
+//   console.log(error, post)
+// })
