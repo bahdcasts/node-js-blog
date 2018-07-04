87ea5ed307262b5483b7bee15ac1903714b5139c
Kati Frantz <frantzenterprises777@gmail.com>
2018-07-04 19:02:42 +0100

    add data flashing

--- controllers/createUser.js
+++ controllers/createUser.js
@@ -1,5 +1,6 @@
 module.exports = (req, res) => {
   res.render('register', {
-    errors: req.flash('registrationErrors')
+    errors: req.flash('registrationErrors'),
+    data: req.flash('data')[0]
   })
 }
--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -6,6 +6,7 @@ module.exports = (req, res) => {
       const registrationErrors = Object.keys(error.errors).map(key => error.errors[key].message)
 
       req.flash('registrationErrors', registrationErrors)
+      req.flash('data', req.body)
       return res.redirect('/auth/register')
     }
     res.redirect('/')
