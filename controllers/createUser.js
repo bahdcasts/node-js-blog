7e0459afee40fcd01aca38c7d995674e7217891e
Kati Frantz <frantzenterprises777@gmail.com>
2018-07-04 18:35:12 +0100

    display list of errors in view

--- controllers/createUser.js
+++ controllers/createUser.js
@@ -1,4 +1,5 @@
 module.exports = (req, res) => {
-  console.log(req.session.registrationErrors)
-  res.render('register')
+  res.render('register', {
+    errors: req.session.registrationErrors
+  })
 }
