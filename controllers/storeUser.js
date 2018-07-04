30f5a7c33dcecf8f0a1d896854f0f653fbe064c4
Kati Frantz <frantzenterprises777@gmail.com>
2018-07-04 12:08:55 +0100

    add error handling

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -2,6 +2,9 @@ const User = require('../database/models/User')
 
 module.exports = (req, res) => {
   User.create(req.body, (error, user) => {
+    if (error) {
+      return res.redirect('/auth/register')
+    }
     res.redirect('/')
   })
 }
