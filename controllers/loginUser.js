6f872c1b1c7641f2ce7f4dc4a690c9ac1bf0ab85
Kati Frantz <frantzenterprises777@gmail.com>
2018-07-04 12:25:16 +0100

    add login process

--- controllers/loginUser.js
+++ controllers/loginUser.js
@@ -1,5 +1,22 @@
+const bcrypt = require('bcrypt')
 const User = require('../database/models/User')
 
 module.exports = (req, res) => {
-  res.redirect('/')
+  const { email, password } = req.body;
+  // try to find the user
+  User.findOne({ email }, (error, user) => {
+    if (user) {
+      // compare passwords.
+      bcrypt.compare(password, user.password, (error, same) => {
+        if (same) {
+          // store user session.
+          res.redirect('/')
+        } else {
+          res.redirect('/auth/login')
+        }
+      })
+    } else { 
+      return res.redirect('/auth/login')
+    }
+  })
 }
