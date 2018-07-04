396fa094ab46a4d9bb859735656298ff34dc3e6f
Kati Frantz <frantzenterprises777@gmail.com>
2018-07-04 09:58:05 +0100

    add user hash hook

--- database/models/User.js
+++ database/models/User.js
@@ -1,9 +1,19 @@
+const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
   username: String,
   email: String,
   password: String
+})
+
+UserSchema.pre('save', function(next) {
+  const user = this
+
+  bcrypt.hash(user.password, 10, function (error, encrypted) {
+    user.password = encrypted
+    next()
+  })
 })
 
 module.exports = mongoose.model('User', UserSchema)
