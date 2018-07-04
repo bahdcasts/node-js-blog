6e3aa6945f3fc936e7b176a4f367a867e00ae0cf
Kati Frantz <frantzenterprises777@gmail.com>
2018-07-04 09:35:17 +0100

    add the view for user registration

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 
-const createPostController = require('./controllers/createPost')
-const homePageController = require('./controllers/homePage')
-const storePostController = require('./controllers/storePost')
-const getPostController = require('./controllers/getPost')
+const createPostController = require("./controllers/createPost");
+const homePageController = require("./controllers/homePage");
+const storePostController = require("./controllers/storePost");
+const getPostController = require("./controllers/getPost");
+const createUserController = require("./controllers/createUser");
 
 const app = new express();
 
@@ -21,14 +22,15 @@ app.set("views", `${__dirname}/views`);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const storePost = require('./middleware/storePost')
+const storePost = require("./middleware/storePost");
 
-app.use('/posts/store', storePost)
+app.use("/posts/store", storePost);
 
 app.get("/", homePageController);
 app.get("/post/:id", getPostController);
 app.get("/posts/new", createPostController);
 app.post("/posts/store", storePostController);
+app.get("/auth/register", createUserController);
 
 app.listen(4000, () => {
   console.log("App listening on port 4000");
