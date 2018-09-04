8cbff7d6d080824dcfb7676296d1fd1de5ada76e
Kati Frantz <frantzenterprises777@gmail.com>
2018-09-04 04:56:54 +0100

    add a wysiwyg editor

--- middleware/storePost.js
+++ middleware/storePost.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-  if(!req.files.image || !req.body.username || !req.body.title || !req.body.subtitle || !req.body.content) {
+  if(!req.files.image || !req.body.title || !req.body.subtitle || !req.body.content) {
     return res.redirect('/posts/new')
   }
 
